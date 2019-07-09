061ae173ef8a7018d15b84315c91e5a81f7d3306
arusak <anton.rusak@gmail.com>
2019-07-09 15:01:27 +0300

    Make the bars vertical

--- js/bar-chart.js
+++ js/bar-chart.js
@@ -1,15 +1,36 @@
 import count from './count.js';
 
 function create(data) {
-    const div = document.createElement('div');
-    Object.entries(count(data)).forEach(([year, count]) => {
-        const item = document.createElement('div');
-        item.className = 'item';
-        item.innerText = `${year}: ${count}`;
-        item.style.width = `${count}px`;
-        div.append(item);
-    });
-    return div;
+    const stats = count(data);
+    const allYears = Object.keys(stats).map(year => +year);
+    const minYear = Math.min(...allYears);
+    const maxYear = Math.max(...allYears);
+
+    const chart = document.createElement('div');
+    chart.className = 'bar-chart';
+
+    for (let y = minYear; y <= maxYear; y++) {
+        const count = stats[y];
+
+        const yearEl = Object.assign(document.createElement('div'), {className: 'item'});
+
+        const barEl = document.createElement('div');
+        barEl.className = 'bar';
+        barEl.style.height = `${count}px`;
+        yearEl.append(barEl);
+
+        const labelEl = document.createElement('div');
+        labelEl.className = 'label';
+        yearEl.append(labelEl);
+
+        if (y % 5 === 0) {
+            labelEl.innerText = `${y}`;
+        }
+
+        chart.append(yearEl);
+    }
+
+    return chart;
 }
 
 export default create;
